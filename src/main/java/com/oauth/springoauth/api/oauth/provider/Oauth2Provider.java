be6427a49a74244f250bc59a6be2d569e7c7267c
kimwanyoung <dhks2869@gmail.com>
2024-11-05 12:58:36 +0900

    feat : 각 Oauth Service를 분리하기 위한 Provider 추가

---
+++ src/main/java/com/oauth/springoauth/api/oauth/provider/Oauth2Provider.java
@@ -0,0 +1,31 @@
+package com.oauth.springoauth.api.oauth.provider;
+
+import com.oauth.springoauth.api.oauth.config.Oauth2Properties;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class Oauth2Provider {
+	private final String clientId;
+	private final String clientSecret;
+	private final String redirectUrl;
+	private final String tokenUrl;
+	private final String userInfoUrl;
+
+	public Oauth2Provider(Oauth2Properties.User user, Oauth2Properties.Provider provider) {
+		this(user.getClientId(), user.getClientSecret(), user.getRedirectUri(), provider.getTokenUri(),
+			provider.getUserInfoUri());
+	}
+
+	@Builder
+	public Oauth2Provider(String clientId, String clientSecret, String redirectUrl, String tokenUrl,
+		String userInfoUrl) {
+		this.clientId = clientId;
+		this.clientSecret = clientSecret;
+		this.redirectUrl = redirectUrl;
+		this.tokenUrl = tokenUrl;
+		this.userInfoUrl = userInfoUrl;
+	}
+}
+
